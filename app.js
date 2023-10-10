9382d6174615a03b2b1176a90d1d3fac4e6a3d68
Praise <praisetoyo@gmail.com>
2023-10-10 09:58:39 -0700

    changes made to app.js using prettier

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const connectDB = require('./db/connect')
-const express = require('express');
+const connectDB = require("./db/connect");
+const express = require("express");
 const app = express();
 
-const mainRouter = require('./routes/main')
-const urlRouter = require('./routes/urls')
+const mainRouter = require("./routes/main");
+const urlRouter = require("./routes/urls");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1/urls', mainRouter)
-app.use('/api/v1', urlRouter)
+app.use("/api/v1/urls", mainRouter);
+app.use("/api/v1", urlRouter);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.send({ msg: "inital commit"})
-})
+app.get("/", (req, res) => {
+  res.send({ msg: "inital commit" });
+});
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () =>
-        console.log(`Server is listening on port ${port}...`)
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
-        console.log(error);
+    console.log(error);
   }
 };
 
