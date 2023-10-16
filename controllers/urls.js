67506063906b8926bdb6519a96de4bec6d0eec3b
PraiseTy <111097676+PraiseTy@users.noreply.github.com>
2023-10-16 01:43:20 -0700

    add post route for creating short url from long url (#3)
    * finished features
    
    * removed unnecessary imports
    
    * fisex stranger error
    
    * created a constant file for shortit url
    
    * added HTTP_ERRORS to constant file
    
    * removed extra space
    
    * removed lines from validation.js file
    
    * removed http-status-code
    
    * changed output given
    
    * updated validation.js file
    
    * updated urls.js in controllers directory
    
    * made changes to the http status in constant file
    
    * added nodemonto devdependencies

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,16 +1,49 @@
-const express = require('express')
-const Url = require('../models/Url')
+const Url = require('../models/Url');
+const generateRandomString = require('../middleware/randomString');
+const { BASEURL, HTTP_ERRORS } = require('../constant');
 
-const createShortUrl = async(req, res) => {
-    const url = await Url.create({...req.body})
-    res.json(url)
-}
+const createShortUrl = async (req, res) => {
+  const { customName, originalUrl } = req.body;
+  let shortUrls;
 
-const customShortUrl = async(req, res)  => {
-    res.send('custom name for short url')
-}
+  try {
+    if (customName) {
+      const customNameWithDashes = customName.split(' ').join('-');
+      shortUrls = `${BASEURL}/${customNameWithDashes}`;
+      const existingCustomName = await Url.findOne({ customName: customNameWithDashes });
+      if (existingCustomName) {
+        return res.status(HTTP_ERRORS.BAD_REQUEST).json({ errors: 'Custom name already exists' });
+      }
+    } else {
+      const randomString = generateRandomString(5);
+      shortUrls = `${BASEURL}/${randomString}`;
+    }
+
+    const existingRandomString = await Url.findOne({ shortUrl: shortUrls });
+    if (existingRandomString) {
+      return res.status(HTTP_ERRORS.BAD_REQUEST).json({ error: 'This string already exists' });
+    }
+    const existingLongUrl = await Url.findOne({ originalUrl });
+    if (existingLongUrl) {
+      return res.status(HTTP_ERRORS.OK).json({ shortUrl: shortUrls, });
+    }
+    const url = await Url.create({
+      ...req.body,
+      shortUrl: shortUrls,
+      customName
+    });
+    return res.status(HTTP_ERRORS.CREATED).json({ message: 'Url shortened succesfully', data: { shortUrl: shortUrls } });
+  } catch (error) {
+    console.error(error);
+    res.status(HTTP_ERRORS.INTERNAL_SERVER_ERROR).json({ error: 'Internal server error. Please try again later.' });
+  }
+};
+
+const customShortUrl = async (req, res) => {
+  res.send('custom name for short url');
+};
 
 module.exports = {
-    createShortUrl,
-    customShortUrl
-}
+  createShortUrl,
+  customShortUrl
+};
---
+++ middleware/validation.js
@@ -0,0 +1,22 @@
+const { body, validationResult } = require('express-validator');
+const { HTTP_ERRORS } = require('../constant');
+
+const validateUrl = [
+  body('originalUrl')
+    .isURL({ protocols: ['http', 'https'], require_tld: true, require_protocol: false })
+    .withMessage('Invalid URL format'),
+  body('customName').optional().isLength({ min: 5 }).withMessage("Custom name must be at least 5 characters")
+];
+
+const validateUrlMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = {
+  validateUrl,
+  validateUrlMiddleware
+};
