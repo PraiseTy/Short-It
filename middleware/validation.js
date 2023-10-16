3877f3e66ea94ad329d191f3c48c39155f4afd89
Praise <praisetoyo@gmail.com>
2023-10-15 23:22:08 -0700

    updated validation.js file

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,11 +4,15 @@ const { HTTP_ERRORS } = require('../constant');
 const validateUrl = [
   body('originalUrl')
     .isURL({ protocols: ['http', 'https'], require_tld: true, require_protocol: false })
-    .withMessage('Invalid URL format')
+    .withMessage('Invalid URL format'),
+  body('customName').optional().isLength({ min: 5 }).withMessage("Custom name must be at least 5 characters")
 ];
 
 const validateUrlMiddleware = (req, res, next) => {
   const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   next();
 };
 
